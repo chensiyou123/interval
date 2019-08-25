1aafe09b60486d93d09f6c44088cd52cb8f1da7b
wujun <13550081270@qq.com>
2019-08-25 18:37:35 +0800

    feat:"首页完成"

--- src/main/java/com/interval/service/controller/MainController.java
+++ src/main/java/com/interval/service/controller/MainController.java
@@ -3,7 +3,6 @@ package com.interval.service.controller;
 import com.interval.service.annotation.SysTimeLog;
 import com.interval.service.component.LockRedisUtils;
 import com.interval.service.enumeration.UserTypeEnum;
-import com.interval.service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,10 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Objects;
 
 /**
  * @Auther: nlife
@@ -43,9 +39,9 @@ public class MainController {
             //查询今日新增用户数
             int todayUser = lockRedisUtils.getNewUserCount();
             //查询今日收入
-             BigDecimal todayIncome = lockRedisUtils.getTodayIncomeCount();
+            BigDecimal todayIncome = lockRedisUtils.getTodayIncomeCount();
             request.setAttribute("todayUser",todayUser);
-              request.setAttribute("todayIncome",todayIncome);
+            request.setAttribute("todayIncome",todayIncome);
             return "index";
         }else{
             request.setAttribute("checkUrl","indexNormal");
